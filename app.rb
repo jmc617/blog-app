2904b3b27fae23c65585fd64a413e84a4022287b
Jessica <jmc617@gmail.com>
2018-05-30 10:43:44 -0400

    signup

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 require 'sinatra/activerecord'
- set :database, 'sqlite3:blogapp.sqlite3'  
+ set :database, 'sqlite3:blogapp.sqlite3'
  set :sessions, true
 require './models'
+
+get "/signup" do
+  erb :'/signup'
+end
+
+post "/create-user" do
+  username = params[:username]
+  password = params[:password]
+  birthdate = params[:birthdate]
+
+  User.create(name: username, password: password, birthdate: birthdate)
+  #change redirect to home when created
+  redirect "/signup"
+end
