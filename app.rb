6a5aa97d3d39695179ab713d32f636f6050f3301
Jessica <jmc617@gmail.com>
2018-05-30 16:26:53 -0400

    profile pages

--- app.rb
+++ app.rb
@@ -1,8 +1,16 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require './models'
+
  set :database, 'sqlite3:blogapp.sqlite3'
  set :sessions, true
-require './models'
+
+ def current_user
+ 	if(session[:user_id])
+ 		@current_user = User.find(session[:user_id])
+ 	end
+ end
+
 
 #homepage
 get '/' do
@@ -31,10 +39,40 @@ end
 post "/create-blog" do
   title = params[:title]
   content = params[:content]
-  date = DateTime.now
+  date = DateTime.now.to_date
 
-  Blog.create(title: title, content: content, date: date)
-
-  redirect "/"
+  if !session[:user_id]
+    redirect '/login'
+  else
+   current_user = User.find(session[:user_id])
+   Blog.create(title: title, content: content, date: date, user_id: current_user.id)
+   redirect "/"
+  end
 end
 
+# Logout functionality
+
+post "/logout" do
+	session[:user_id] = nil
+	redirect "/"
+end
+
+get "/login" do
+	erb :'users/login'
+end
+
+post "/login" do
+	user = User.where(name: params[:name]).first
+	if user.password == params[:password]
+		session[:user_id] = user.id
+		redirect "/"
+	else
+		redirect "/login"
+  end
+end
+
+get "/show/:id/profile" do
+@user = User.find(params[:id])
+# @blogs = @user.blogs
+erb :'/users/profile'
+end
